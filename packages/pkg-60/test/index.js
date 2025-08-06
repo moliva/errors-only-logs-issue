b1b9af93623b09ec701e49773c4cd7017f2985af
Miguel Oliva <miguel@phantom.app>
2025-08-06 11:51:48 -0300

    adding packages and tests

---
+++ packages/pkg-60/test/index.js
@@ -0,0 +1,22 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+describe("test suite 1", () => {
+  it("example 1", () => {
+    assert.strictEqual(true, true);
+  });
+
+  it("example 2", () => {
+    assert.strictEqual(true, true);
+  });
+});
+
+describe("test suite 2", () => {
+  it("example 1", () => {
+    assert.strictEqual(true, true);
+  });
+
+  it("example 2", () => {
+    assert.strictEqual(true, true);
+  });
+});
